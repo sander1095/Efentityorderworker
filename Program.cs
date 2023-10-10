250d8842d9696acb84086daf46e0ca8bd39d4aca
Sander ten Brinke <s.tenbrinke2@gmail.com>
2023-10-10 14:21:12 +0200

    set up project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using Efentityorderworker;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
